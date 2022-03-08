9b0e91ca234df06eb3261fe80baaae8d9abe2021
Scott Muc <scott@scottmuc.com>
2022-03-08 11:37:57 +0100

    Fix docker run command

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@
 set -e
 
 docker build --tag dns-zone-test .
-docker run --rm dns-zone-test \
+
+docker run --rm
     -v $PWD:/dns-zone-blocklist \
     -w /dns-zone-blocklist \
+    dns-zone-test \
     ./check-config-tests.sh
